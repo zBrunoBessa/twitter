ac9d23aa43ce00c7ae5efaebbb7e9e5aa1d0df87
Bruno Bessa <beduardo2014@gmail.com>
2024-08-19 09:49:44 -0300

    feat: follow and unfollow buttons

--- twitter/views.py
+++ twitter/views.py
@@ -17,6 +17,14 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
         return render(request, "profile.html", {"profile":profile})
     else:
         messages.success(request, ("You Must Be Logged In To View This Page..."))
